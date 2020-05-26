9af3bef430dd0b6b5c6b9c756fcb8ad7ec2f9831
Rishab <rishabarora2008@Gmail.com>
2020-05-26 13:15:55 +0530

    init commit

---
+++ webhook.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const http = require('http');
+
+const app = express();
+const server = new http.Server(app);
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.post('/', (req, res) => {
+    const { queryResult } = req.body;
+    if (queryResult.action === 'convert') {
+        const { outputCurrency, amountToConvert } = queryResult.parameters;
+
+        if (amountToConvert.currency === outputCurrency) {
+            const { amount, currency } = amountToConvert;
+            const response = `Well ${amount} ${currency} is obviously equal to ${amount} ${currency}`;
+            res.send({
+                speech: response,
+                displayText: response,
+                source: 'moneybot-webhook'
+            });
+        }
+    }
+});
+
+server.listen(port, () => {
+    console.log(`Running on port ${port}`);
+});
