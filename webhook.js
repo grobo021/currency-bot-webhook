9d82a368a3ec5d6f231ac212d2dd1d609edf1366
Rishab <rishabarora2008@Gmail.com>
2020-05-26 18:42:37 +0530

    actual currency conversion

--- webhook.js
+++ webhook.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 const http = require('http');
 
 const app = express();
@@ -7,37 +8,52 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    console.log('####### App Started ######');
-    res.send('App started');
-});
+const convertCurrency = async ({ amount, currency }, outputCurrency, cb) => {
+  try {
+    const data = await JSON.parse(axios.get(`http://data.fixer.io/api/latest?access_key=${process.env.ACCESS_KEY}&base=${currency}&symbols=${outputCurrency}`));
+    const computedValue = Math.round(data.body.rates[outputCurrency] * amount);
+    cb(null, `${amount} ${currency} converts to about ${outputCurrency} ${computedValue} as per current rates!`);
+  } catch (e) {
+    cb(e, null);
+  }
+};
 
 app.post('/', (req, res) => {
-    const { queryResult } = req.body;
-    console.log(`1111111 ${queryResult} 1111111`);
-    if (queryResult.action === 'convert') {
-        const { outputCurrency, amountToConvert } = queryResult.parameters;
-        console.log(`2222222 ${queryResult.parameters} 2222222`);
+  const { queryResult } = req.body;
+  if (queryResult.action === 'convert') {
+    const { outputCurrency, amountToConvert } = queryResult.parameters;
 
-        if (amountToConvert.currency === outputCurrency) {
-            const { amount, currency } = amountToConvert;
-            const response = `Well ${amount} ${currency} is obviously equal to ${amount} ${currency}`;
-            console.log(`3333333 ${response} 3333333`);
-            res.send({
-                fulfillmentMessages: [
-                  {
-                    text: {
-                      text: [
-                        response
-                      ]
-                    }
-                  }
-                ]
-              });
-        }
+    if (amountToConvert.currency === outputCurrency) {
+      const { amount, currency } = amountToConvert;
+      const response = `Well ${amount} ${currency} is obviously equal to ${amount} ${currency}`;
+      res.send({
+        fulfillmentMessages: [
+          {
+            text: {
+              text: [
+                response
+              ]
+            }
+          }
+        ]
+      });
     }
-});
+  } else {
+      res.send({
+        fulfillmentMessages: [
+          {
+            text: {
+              text: [
+                convertCurrency(amountToConvert, outputCurrency)
+              ]
+            }
+          }
+        ]
+      });
+    }
+  }
+);
 
 server.listen(port, () => {
-    console.log(`Running on port ${port}`);
+  console.log(`Running on port ${port}`);
 });
